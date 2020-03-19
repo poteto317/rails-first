314040c32b45b74e5f543720b7ba35667b55d0a3
github poteto317 <msyksowk@gmail.com>
2020-03-19 16:57:26 +0900

    bcryptによるパスワードのハッシュ化

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_071811) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password_dijest", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
